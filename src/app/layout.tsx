0fc15239ec0c459f583024f9f1f61d54bf7bcda0
sehyeon <boksil211@naver.com>
2024-03-04 18:31:21 +0900

    [style]초기세팅

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "데브캠프 로그인/회원가입",
+  description: "데브캠프 로그인/회원가입 프론트앤드 구현",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
