172669ca14ba8b24aedad21711466003e56fddfe
sehyeon <boksil211@naver.com>
2024-03-08 03:43:15 +0900

    [doc] 파일이름 수정

--- src/validators/auth.ts
+++ src/validators/auth.ts
@@ -20,19 +20,19 @@ export const registerSchema = z.object({
   password: z
     .string()
     .min(6, "비밀번호는 최소 6자리 이상이어야 합니다.")
-    .max(20, "비밀번호는 20자리 이하이어야 합니다.")
-    .refine((value) => passwordRegex.test(value), {
-      message:
-        "비밀번호는 최소 6자리 이상, 영문, 숫자, 특수문자를 포함해야 합니다.",
-    }),
+    .max(100, "비밀번호는 100자리 이하이어야 합니다.")
+    .refine(
+      (value) => passwordRegex.test(value),
+      "비밀번호는 최소 6자리 이상, 영문, 숫자, 특수문자를 포함해야 합니다."
+    ),
   confirmPassword: z
     .string()
     .min(6, "비밀번호는 최소 6자리 이상이어야 합니다.")
-    .max(20, "비밀번호는 20자리 이하이어야 합니다.")
-    .refine((value) => passwordRegex.test(value), {
-      message:
-        "비밀번호는 최소 6자리 이상, 영문, 숫자, 특수문자를 포함해야 합니다.",
-    }),
+    .max(100, "비밀번호는 100자리 이하이어야 합니다.")
+    .refine(
+      (value) => passwordRegex.test(value),
+      "비밀번호는 최소 6자리 이상, 영문, 숫자, 특수문자를 포함해야 합니다."
+    ),
 });
 
 export type TsRegisterSchemaType = z.infer<typeof registerSchema>; // 타입 추론 자동
