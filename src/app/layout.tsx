5fd36e52f93bb55c97f4ac88c4df5cdae1f5cd99
sehyeon <boksil211@naver.com>
2024-03-07 11:18:42 +0900

    [feat]로그인 페이지 생성  공용 헤더 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,16 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +26,52 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="kr">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Menubar>
+          <MenubarMenu>
+            <MenubarTrigger>
+              <Link href="/">홈</Link>
+            </MenubarTrigger>
+            <MenubarContent>
+              <MenubarItem>
+                New Tab <MenubarShortcut>바로가기</MenubarShortcut>
+              </MenubarItem>
+            </MenubarContent>
+          </MenubarMenu>
+          <MenubarMenu>
+            <MenubarTrigger>
+              <Link href="/">회원가입</Link>
+            </MenubarTrigger>
+            <MenubarContent>
+              <MenubarItem>
+                New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+              </MenubarItem>
+            </MenubarContent>
+          </MenubarMenu>
+          <MenubarMenu>
+            <MenubarTrigger>
+              <Link href="/login">로그인</Link>
+            </MenubarTrigger>
+            <MenubarContent>
+              <MenubarItem>
+                New Tab <MenubarShortcut>바로가기</MenubarShortcut>
+              </MenubarItem>
+            </MenubarContent>
+          </MenubarMenu>
+          <MenubarMenu>
+            <MenubarTrigger>
+              <Link href="/cart">장바구니</Link>
+            </MenubarTrigger>
+            <MenubarContent>
+              <MenubarItem>
+                New Tab <MenubarShortcut>바로가기</MenubarShortcut>
+              </MenubarItem>
+            </MenubarContent>
+          </MenubarMenu>
+        </Menubar>
+
+        {children}
+      </body>
     </html>
   );
 }
